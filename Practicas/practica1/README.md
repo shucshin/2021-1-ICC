8bde059d303be873fb8e1790982280688133f2cf
shucshin <70228900+shucshin@users.noreply.github.com>
2021-02-08 08:30:54 -0600

    Add files via upload

---
+++ Practicas/practica1/README.md
@@ -0,0 +1,49 @@
+Introducción a Ciencias de la Computación
+=========================================
+
+Práctica 1: Orientación a Objetos y sintaxis de Java
+----------------------------------------------------
+
+### Fecha de entrega: martes 13 de octubre, 2020
+
+Deben implementar los métodos faltantes de la clase 
+[Estudiante](https://aztlan.fciencias.unam.mx/gitlab/2021-1-icc/practica1/blob/master/src/main/java/mx/unam/ciencias/icc/Estudiante.java).
+
+Una vez que hayan terminado su clase, debe compilar al hacer:
+
+```
+$ mvn compile
+```
+
+También debe pasar todas sus pruebas unitarias al hacer:
+
+```
+$ mvn test
+```
+
+Por último, debe ejecutar correctamente el programa escrito en la clase
+[Practica1](https://aztlan.fciencias.unam.mx/gitlab/2021-1-icc/practica1/blob/master/src/main/java/mx/unam/ciencias/icc/Practica1.java)
+al ejecutar:
+
+```
+$ mvn install
+...
+$ java -jar target/practica1.jar
+```
+
+El único archivo que deben modificar es `Estudiante.java`. *No deben modificar
+de ninguna manera ninguno de los otros archivos de la práctica*.
+
+### Repositorio
+
+Pueden clonar la práctica con el siguiente comando:
+
+```
+$ git clone https://canek@aztlan.fciencias.unam.mx/gitlab/2021-1-icc/practica1.git
+```
+
+### Documentación
+
+La documentación generada por JavaDoc la pueden consultar aquí:
+
+[Documentación generada por JavaDoc para la práctica 1](https://aztlan.fciencias.unam.mx/~canek/2021-1-icc/practica1/apidocs/index.html)
---
+++ Practicas/practica1/src/main/java/mx/unam/ciencias/icc/Estudiante.java
@@ -0,0 +1,134 @@
+package mx.unam.ciencias.icc;
+
+/**
+ * Clase para representar estudiantes. Un estudiante tiene nombre, número de
+ * cuenta, promedio y edad.
+ */
+public class Estudiante {
+
+    /* Nombre del estudiante. */
+    private String nombre;
+    /* Número de cuenta. */
+    private int cuenta;
+    /* Pormedio del estudiante. */
+    private double promedio;
+    /* Edad del estudiante.*/
+    private int edad;
+
+    /**
+     * Define el estado inicial de un estudiante.
+     * @param nombre el nombre del estudiante.
+     * @param cuenta el número de cuenta del estudiante.
+     * @param promedio el promedio del estudiante.
+     * @param edad la edad del estudiante.
+     */
+    public Estudiante(String nombre,
+                      int    cuenta,
+                      double promedio,
+                      int    edad) {
+        this.nombre    = nombre;
+        // Aquí va su código.
+        this.cuenta    = cuenta;
+        this.promedio  = promedio;
+        this.edad      = edad;
+    }
+
+    /**
+     * Regresa el nombre del estudiante.
+     * @return el nombre del estudiante.
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * Define el nombre del estudiante.
+     * @param nombre el nuevo nombre del estudiante.
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * Regresa el número de cuenta del estudiante.
+     * @return el número de cuenta del estudiante.
+     */
+    public int getCuenta() {
+        // Aquí va su código.
+        return cuenta;
+    }
+
+    /**
+     * Define el número cuenta del estudiante.
+     * @param cuenta el nuevo número de cuenta del estudiante.
+     */
+    public void setCuenta(int cuenta) {
+        // Aquí va su código.
+        this.cuenta = cuenta;
+    }
+
+    /**
+     * Regresa el promedio del estudiante.
+     * @return el promedio del estudiante.
+     */
+    public double getPromedio() {
+        // Aquí va su código.
+        return promedio;
+    }
+
+    /**
+     * Define el promedio del estudiante.
+     * @param promedio el nuevo promedio del estudiante.
+     */
+    public void setPromedio(double promedio) {
+        // Aquí va su código.
+        this.promedio = promedio;
+    }
+
+    /**
+     * Regresa la edad del estudiante.
+     * @return la edad del estudiante.
+     */
+    public int getEdad() {
+        // Aquí va su código.
+        return edad;
+    }
+
+    /**
+     * Define la edad del estudiante.
+     * @param edad la nueva edad del estudiante.
+     */
+    public void setEdad(int edad) {
+        // Aquí va su código.
+        this.edad = edad;
+    }
+
+    /**
+     * Regresa una representación en cadena del estudiante.
+     * @return una representación en cadena del estudiante.
+     */
+    public String toString() {
+        // Aquí va su código.
+        String cadenaEstudiante = String.format("Nombre   : %s\n" + "Cuenta   : %09d\n" + "Promedio : %2.2f\n" + "Edad     : %d", nombre, cuenta, promedio, edad);
+        return cadenaEstudiante;
+    }
+
+    /**
+     * Nos dice si el estudiante recibido es igual al que manda llamar el
+     * método.
+     * @param estudiante el estudiante con el cual comparar.
+     * @return <code>true</code> si el estudiante recibido tiene las mismas
+     *         propiedades que el estudiante que manda llamar al método,
+     *         <code>false</code> en otro caso.
+     */
+    public boolean equals(Estudiante estudiante) {
+        if (!(estudiante instanceof Estudiante))
+            return false;
+        // Aquí va su código.
+        Estudiante a = (Estudiante) estudiante;
+        return this.nombre.equals(a.nombre) && this.cuenta == a.cuenta && this.promedio == a.promedio && this.edad == a.edad;
+        
+        
+
+    }
+}
